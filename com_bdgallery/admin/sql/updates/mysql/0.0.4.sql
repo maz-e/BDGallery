8fff7b7ba70d8f3d11cc80dcbd5cc098d8853668
Miguel Angel <m.zapes@gmail.com>
2015-08-19 19:06:27 +0200

    Adding database files

---
+++ com_bdgallery/admin/sql/updates/mysql/0.0.4.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS `#__bdgallery`;
+
+CREATE TABLE `#__bdgallery` (
+	`id`       INT(11)     NOT NULL AUTO_INCREMENT,
+	`album` VARCHAR(255) NOT NULL,
+   `description` VARCHAR(255) NULL,
+   `totalimg` INT(11) NOT NULL DEFAULT 0,
+   `folderlist` VARCHAR(255) NULL,
+   `imgfolder` VARCHAR(255) NULL,
+   `catid`	    int(11)    NOT NULL DEFAULT '0',
+	`published` tinyint(4) NOT NULL,
+	PRIMARY KEY (`id`)
+)
+	ENGINE =MyISAM
+	AUTO_INCREMENT =0
+	DEFAULT CHARSET =utf8;
+
+INSERT INTO `#__bdgallery` (`album`) VALUES
+('Casa Ruso'),
+('Suelo Radiante'),
+('Placas Solares');
