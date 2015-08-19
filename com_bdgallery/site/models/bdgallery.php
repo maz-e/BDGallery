c57b7858fd79bd14723baa7f196a56c736649366
Miguel Angel <m.zapes@gmail.com>
2015-08-19 17:50:47 +0200

    Correct the bad joomla model

--- com_bdgallery/site/models/bdgallery.php
+++ com_bdgallery/site/models/bdgallery.php
@@ -15,7 +15,7 @@ defined('_JEXEC') or die('Restricted access');
  *
  * @since  0.0.1
  */
-class BdGalleryModelBdGallery extends JModelListItem
+class BdGalleryModelBdGallery extends JModelList
 {
 	/**
 	 * @var string albums
