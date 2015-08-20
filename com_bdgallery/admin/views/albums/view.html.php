d039a54e85c39b462d5b02756197c8b2aaf5cb01
Miguel Angel <m.zapes@gmail.com>
2015-08-20 14:19:03 +0200

    Basic backend
    Create the view

---
+++ com_bdgallery/admin/views/albums/view.html.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @package     Joomla.Bladis
+ * @subpackage  com_bdgallery
+ *
+ * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * Albums View
+ *
+ * @since  0.0.1
+ */
+class BdGalleryViewAlbums extends JViewLegacy
+{
+	/**
+	 * Display the Albums view
+	 *
+	 * @param   string  $tpl  The name of the template file to parse; automatically searches through the template paths.
+	 *
+	 * @return  void
+	 */
+	function display($tpl = null)
+	{
+		// Get data from the model
+		$this->items		= $this->get('Items');
+		$this->pagination	= $this->get('Pagination');
+
+		// Check for errors.
+		if (count($errors = $this->get('Errors')))
+		{
+			JError::raiseError(500, implode('<br />', $errors));
+
+			return false;
+		}
+
+		// Display the template
+		parent::display($tpl);
+	}
+}
