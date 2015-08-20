02e76ebe6925ef8860f076cdcdb08b4369769264
Miguel Angel <m.zapes@gmail.com>
2015-08-20 15:02:18 +0200

    Adding backend actions
    Adding specific controllers

---
+++ com_bdgallery/admin/controllers/bdgallery.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package     Joomla.Bladis
+ * @subpackage  com_bdgallery
+ *
+ * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * BdGallery Controller
+ *
+ * @package     Joomla.Bladis
+ * @subpackage  com_bdgallery
+ * @since       0.0.8
+ */
+class BdGalleryControllerBdGallery extends JControllerForm
+{
+}
