e72fcb82a2228507bf1b6c72e4802e0beaedc798
Miguel Angel <m.zapes@gmail.com>
2015-08-20 14:22:39 +0200

    Basic backend
    Create the model

---
+++ com_bdgallery/admin/models/albums.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @package     Joomla.Bladis
+ * @subpackage  com_bdgallery
+ *
+ * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * AlbumList Model
+ *
+ * @since  0.0.1
+ */
+class BdGalleryModelAlbums extends JModelList
+{
+	/**
+	 * Method to build an SQL query to load the list data.
+	 *
+	 * @return      string  An SQL query
+	 */
+	protected function getListQuery()
+	{
+		// Initialize variables.
+		$db    = JFactory::getDbo();
+		$query = $db->getQuery(true);
+
+		// Create the base select statement.
+		$query->select('*')
+                ->from($db->quoteName('#__bdgallery'));
+
+		return $query;
+	}
+}
