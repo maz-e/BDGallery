7613e4bb86cbc0939943880673ace5fd34c34777
Miguel Angel <m.zapes@gmail.com>
2015-08-20 15:11:09 +0200

    Adding backend actions
    Adding an editing view

---
+++ com_bdgallery/admin/views/bdgallery/view.html.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * @package     Joomla.Bladis
+ * @subpackage  com_bdgallery
+ *
+ * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * BdGallery View
+ *
+ * @since  0.0.1
+ */
+class BdGalleryViewBdGallery extends JViewLegacy
+{
+	/**
+	 * View form
+	 *
+	 * @var         form
+	 */
+	protected $form = null;
+
+	/**
+	 * Display the BdGallery view
+	 *
+	 * @param   string  $tpl  The name of the template file to parse; automatically searches through the template paths.
+	 *
+	 * @return  void
+	 */
+	public function display($tpl = null)
+	{
+		// Get the Data
+		$this->form = $this->get('Form');
+		$this->item = $this->get('Item');
+
+		// Check for errors.
+		if (count($errors = $this->get('Errors')))
+		{
+			JError::raiseError(500, implode('<br />', $errors));
+
+			return false;
+		}
+
+
+		// Set the toolbar
+		$this->addToolBar();
+
+		// Display the template
+		parent::display($tpl);
+	}
+
+	/**
+	 * Add the page title and toolbar.
+	 *
+	 * @return  void
+	 *
+	 * @since   1.6
+	 */
+	protected function addToolBar()
+	{
+		$input = JFactory::getApplication()->input;
+
+		// Hide Joomla Administrator Main menu
+		$input->set('hidemainmenu', true);
+
+		$isNew = ($this->item->id == 0);
+
+		if ($isNew)
+		{
+			$title = JText::_('COM_BDGALLERY_MANAGER_BDGALLERY_NEW');
+		}
+		else
+		{
+			$title = JText::_('COM_BDGALLERY_MANAGER_BDGALLERY_EDIT');
+		}
+
+		JToolBarHelper::title($title, 'bdgallery');
+		JToolBarHelper::save('bdgallery.save');
+		JToolBarHelper::cancel(
+			'bdgallery.cancel',
+			$isNew ? 'JTOOLBAR_CANCEL' : 'JTOOLBAR_CLOSE'
+		);
+	}
+}
